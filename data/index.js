06924743d0997eb14d6035a706a66900e4a8ae05
farrel216 <farrel.arp@gmail.com>
2023-09-19 15:26:26 +0700

    add data

---
+++ data/index.js
@@ -0,0 +1,13 @@
+const trendSerdos = require("./trend_serdos.json");
+const trendDosen = require("./trend_dosen.json");
+const trendTendik = require("./trend_tendik.json");
+const jumlahDosen = require("./jumlah_dosen.json");
+const jumlahTendik = require("./jumlah_tendik.json");
+
+module.exports = {
+  trendDosen,
+  trendSerdos,
+  trendTendik,
+  jumlahDosen,
+  jumlahTendik,
+};
--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,34 @@
 var express = require("express");
 var router = express.Router();
+const {
+  trendSerdos,
+  trendDosen,
+  trendTendik,
+  jumlahTendik,
+  jumlahDosen,
+} = require("../data");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.send("Akses api berhasil");
+});
+
+router.get("/dosen/jumlah", (req, res) => {
+  res.send(jumlahDosen).status(200);
+});
+router.get("/dosen/trend", (req, res) => {
+  res.send(trendDosen).status(200);
+});
+
+router.get("/tendik/jumlah", (req, res) => {
+  res.send(jumlahTendik).status(200);
+});
+router.get("/tendik/trend", (req, res) => {
+  res.send(trendTendik).status(200);
+});
+
+router.get("/serdos/trend", (req, res) => {
+  res.send(trendSerdos).status(200);
 });
 
 module.exports = router;
